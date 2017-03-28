53ec17bac1a2ddfe6c267aa210ac3624d2282b60
Lee Matabire <Matabire23@gmail.com>
2017-03-28 11:15:02 -0700

    color change

--- bundle/scripts.js
+++ bundle/scripts.js
@@ -1,3 +1,17 @@
 "use strict";
 
-var b = 3;
+function getRandomNum() {
+  var randomNum = Math.floor(Math.random() * (10 - 0) + 1);
+  document.getElementById("result").append(randomNum);
+};
+
+function changeColor() {
+  var colors = ["yellow", "red", "green"];
+
+  setInterval(function () {
+    var elem = document.getElementById("result");
+    elem.className = colors[Math.floor(Math.random() * colors.length)];
+  }, 1000);
+};
+changeColor();
+document.getElementById("button").addEventListener('click', getRandomNum);
--- scripts/script.es6.js
+++ scripts/script.es6.js
@@ -1,1 +1,17 @@
-const b = 3;
+function getRandomNum() {
+  let randomNum = Math.floor(Math.random() * (10 - 0) + 1);
+  document.getElementById("result"). append(randomNum);
+};
+
+
+function changeColor() {
+  let colors = ["yellow", "red", "green"]
+
+
+setInterval(() =>{
+  let elem = document.getElementById("result")
+  elem.className = colors[Math.floor(Math.random() * colors.length)];
+}, 1000);
+};
+changeColor();
+document.getElementById("button").addEventListener('click', getRandomNum);
